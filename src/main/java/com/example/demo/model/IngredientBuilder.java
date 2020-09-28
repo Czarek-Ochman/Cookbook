5ad7c2b738a2fbdb6496c74225fc15da3b56d5f7
Czarek <czarek.ochman@gmail.com>
2020-09-28 14:58:03 +0200

    Adding the remaining files

---
+++ src/main/java/com/example/demo/model/IngredientBuilder.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class IngredientBuilder {
+    private String name;
+    private String amount;
+    private Long idRecipe;
+}
